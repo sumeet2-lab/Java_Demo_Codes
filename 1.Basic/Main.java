495cbf7ec3ec9984f35c0b75986ce40714e61da0
sumeet <sumeetjain.com01@gmail.com>
2022-03-13 17:43:03 +0530

    first

---
+++ 1.Basic/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a=0;
+        System.out.println(a +" hello ji");
+        System.out.println("*");
+
+        
+    }
+}
