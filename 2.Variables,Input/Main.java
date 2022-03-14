7e2cd82db4a3405dfcb9af5f57ad4fd95928ed0d
sumeet <sumeetjain.com01@gmail.com>
2022-03-14 20:06:38 +0530

    second

---
+++ 2.Variables,Input/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+        public static void main(String[] args) {
+          Scanner input = new Scanner(System.in);
+          System.out.println("Enter Your name- ");
+          String a = input.next();
+          System.out.println(a);
+        }
+
+
+}
